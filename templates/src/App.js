1af5da0c136be3ffaeee43081615c0ab217dd344
vengeance-404 <alokkumardas610@gmail.com>
2026-02-17 19:21:35 +0530

    Update App.js

--- templates/src/App.js
+++ templates/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import Scanner from './Scanner'; // We import the scanner here
 import './App.css';
